1251fc029de1e21003d200ed3d5e635cdb9d2081
saurovsergey <saurov.sergej@qubeek.io>
2020-03-24 14:47:44 +0300

    Update MdashServiceProvider.php

--- src/MdashServiceProvider.php
+++ src/MdashServiceProvider.php
@@ -13,11 +13,6 @@ class MdashServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'qubeek');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'qubeek');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
@@ -60,23 +55,5 @@ class MdashServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/mdash.php' => config_path('mdash.php'),
         ], 'mdash.config');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/qubeek'),
-        ], 'mdash.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/qubeek'),
-        ], 'mdash.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/qubeek'),
-        ], 'mdash.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
